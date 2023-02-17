ac025ffd856760a32bf58c0950a63e74b313dbbc
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-17 20:18:17 +0400

    up

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - do the main job
+ * Return: Zero upon finishing
+ */
+int	main(void)
+{
+	char	c = 'z';
+
+	while (c <= 'a')
+		putchar(c--);
+	putchar('\n');
+	return (0);
+}
