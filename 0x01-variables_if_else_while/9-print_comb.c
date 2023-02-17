a0dc820b9c99d020bf82221258305c15233eda2d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-17 20:35:58 +0400

    up

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - do the main job
+ * Return: Zero upon finishing
+ */
+int	main(void)
+{
+	char	c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c++);
+		if (c != '9' + 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
