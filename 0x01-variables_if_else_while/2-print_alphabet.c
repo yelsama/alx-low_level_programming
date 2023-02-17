6a76e068242a893a18d4a64fa759f96735752a87
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-17 19:35:10 +0400

    up

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - do the main job
+ * Return: Zero upon finishing
+ */
+int	main(void)
+{
+	char	c='a';
+
+	while (c++ <= 'z')
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
