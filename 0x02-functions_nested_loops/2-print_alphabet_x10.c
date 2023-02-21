fdb4deb971c61511f91e372831280188481d33c5
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-02-21 19:58:21 +0400

    up

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet  - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphabet_x10(void)
+{
+	in	i;
+
+	i = -1;
+	while (++i < 10)
+		print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 # include <stdio.h>
 
 int	print_alphabet(void);
+int	print_alphabet_x10(void);
 #endif
