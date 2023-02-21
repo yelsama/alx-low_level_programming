29a16059cda02a1b09ebd336f6637f489516283b
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-02-22 00:20:07 +0400

    up

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer  - check the code
+ */
+void	jack_bauer(void)
+{
+	int	h;
+	int	m;
+
+	h = -1;
+	while (++h < 24)
+	{
+		m = -1;
+		while (++m < 61)
+		{
+
+			putchar((h / 10) + '0');
+			putchar((h % 10) + '0');
+			putchar(':');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+		}
+	}
+}
