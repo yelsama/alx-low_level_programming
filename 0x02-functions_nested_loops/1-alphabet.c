6a6e6f57db15a2101afc919b965a411d72432bc6
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-02-21 19:35:34 +0400

    up

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'y')
+		putchar(c++);
+	puchar('\n');
+	return (0);
+}
