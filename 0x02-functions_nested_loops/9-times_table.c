2af46bdcff5880fa7404ec4ec063296299a62590
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-02-22 00:53:21 +0400

    up

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ */
+void	times_table(void)
+{
+	int	n;
+	int	m;
+	int	tmp;
+
+	n = -1;
+	while (++n < 10)
+	{
+		m = -1;
+		while (++m < 13)
+		{
+			tmp = n * m;
+			if (tmp > 9)
+				putchar((tmp / 10) + '0');
+			putchar((tmp % 10) + '0');
+			if (m != 12)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+}
