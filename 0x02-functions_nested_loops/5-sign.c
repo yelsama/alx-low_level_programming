8fba90086fb101cc6bf7d797a2c25ebf970b1127
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-02-21 20:41:36 +0400

    up

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign  - check the code
+ * @n: the given number to check
+ * Return: 1 if given number is positive
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return(putchar('+'), 1);
+	if (n < 0)
+		return(putchar('-'), -1);
+	return (putchar('-'), 0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int	print_alphabet_x10(void);
 int	_islower(char c);
 int	_putchar(char c);
 int	_isalpha(int c);
+int	print_sign(int n);
 #endif
