b494c378dbaa415a68fbb06183b3bcf4c23d65cf
Yassein Idris <yasseinidris@Yasseins-MacBook-Air.local>
2023-02-21 20:54:04 +0400

    up

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,9 +8,15 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		return(printf("+"), 1);
+	{
+		puthchar('+');
+		return (1);
+	}
 	if (n < 0)
-		return(printf("-"), -1);
-	return (printf("0"), 0);
+	{
+		putcahr('-');
+		return (-1);
+	}
+	putchar ('0');
+	return (0);
 }
-
