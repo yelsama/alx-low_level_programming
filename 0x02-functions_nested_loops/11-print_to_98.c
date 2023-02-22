c43165a0048a83073b09cefb6dea395c61e218a0
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-22 17:59:24 +0400

    up

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_to_98  - check the code
+ * @n: the number to print from
+ */
+void print_sign(int n)
+{
+	while (n != 98)
+	{
+		if (n < 98)
+			printf("%d, ", n++)
+		if (n > 98)
+			printf("%d, ", n--);
+	}
+	printf("98\n");
+}
