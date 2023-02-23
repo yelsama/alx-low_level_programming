e943b2ffc0cad9d1b4d346cb0a6d434e69188ea3
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s6.42abudhabi.ae>
2023-02-23 22:46:16 +0400

    up

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_most_numbers - check the code
+ *
+ */
+void  print_most_numbers(void)
+{
+	char	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		if (c != '2' || c != '4')
+			_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int		_isdigit(int c);
 int		mul(int a, int b);
 void	print_numbers(void);
 void	_putchar(char);
+void 	print_most_numbers(void);
 #endif
