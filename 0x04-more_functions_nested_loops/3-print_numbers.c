0c7ecd804117f8872dcb0c140f86efd472eb35c4
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s6.42abudhabi.ae>
2023-02-23 22:01:00 +0400

    up

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ */
+void  print_numbers(void)
+{
+	char	c;
+
+	c = '0';
+	while (c <= '9')
+		_putchar(c++);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 # define MAIN_H
 # include <stdio.h>
 
-int	_isupper(int c);
-int	_isdigit(int c);
-int	mul(int a, int b);
+int		_isupper(int c);
+int		_isdigit(int c);
+int		mul(int a, int b);
+void	print_numbers(void);
 #endif
