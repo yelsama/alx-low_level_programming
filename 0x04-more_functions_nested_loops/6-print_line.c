3a45a22d1d1d40d4b00813ace62c28c59a203c04
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s6.42abudhabi.ae>
2023-02-23 23:20:00 +0400

    up

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: number of times the line should be printed
+ */
+void  print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
