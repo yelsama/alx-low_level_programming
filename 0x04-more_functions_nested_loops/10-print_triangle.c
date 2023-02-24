bd42aed7c82152053e58fa54d4a59b5651968f0c
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-24 18:00:47 +0400

    up

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size: triangle base using hashs
+ */
+void  print_triangle(int size)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	j = 0;
+	while (++j <= size)
+	{
+		i = size + 1;
+		k = -1;
+		while (--i > j)
+			_putchar(' ');
+		while (++k < size)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}
