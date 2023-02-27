d3c888a927dea17e10764a02d860bbafd2dcdf4c
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-27 14:55:49 +0400

    up

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ * @s: a string to reverse
+ */
+void	rev_string(char *s)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	c;
+
+	if (!s)
+		return;
+	i = 0;
+	while (s[i])
+		i++;
+	j = i / 2;
+	k = 0;
+	while (k++ < j)
+	{
+		c = s[k];
+		s[k] = s[--i];
+		s[i] = c;
+	}
+}
