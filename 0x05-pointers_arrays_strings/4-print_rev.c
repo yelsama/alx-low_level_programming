97ad4a12b700e960d1ae8c4ab3321ebafda0f86c
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-27 14:45:14 +0400

    up

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: a string to print
+ */
+void	print_rev(char *s)
+{
+	int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+		i++;
+	while (i > 0)
+		write(1, &s[--i], 1);
+	write(1, "\n", 1);
+}
