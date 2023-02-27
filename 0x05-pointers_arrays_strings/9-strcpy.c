252b9185ad17462e6fc93d98958173153edb5a1d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-27 15:51:17 +0400

    up

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - check the code
+ * @dest: pointer to where should be printed
+ * @src: pointer to the original string
+ */
+char	*_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	if (!dest || !src)
+		return (NULL);
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = src[i];
+	return (dest);
+}
