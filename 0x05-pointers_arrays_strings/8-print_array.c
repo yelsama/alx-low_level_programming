fbc623ebb7a490e2e1b680ade9b2fbe7e3a308a4
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-02-27 15:32:08 +0400

    up

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - check the code
+ * @a: an array of intgers to be printed
+ * @n: number of elements to be printed
+ */
+void	print_array(int *a, int n)
+{
+	int	i;
+
+	if (!a)
+		return;
+	i = -1;
+	while (++i < n -1)
+		printf("%d, ", a[i]);
+	if (n > 0)
+		printf("%d", a[i]);
+	printf("\n");
+}
