9d7a69392d98f10e04ef12399672d672c34e27f6
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r5s12.42abudhabi.ae>
2023-03-01 20:39:18 +0400

    up

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ * @dest: a placr to copy
+ * @src: place to take string from
+ * Return: the destenation
+ */
+char	*_strcat(char *dest, char *src)
+{
+	int	i;
+	int	n;
+
+	if (!dest || !src)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
