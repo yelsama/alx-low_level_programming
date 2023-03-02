aae06a368e599c84b923ed79f85e08630a9f7305
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-02 19:24:21 +0400

    up

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,10 @@ char	*_strncpy(char *dest, char *src, int n)
 	int	i;
 
 	i = 0;
-	if (n < 1)
-		return (dest);
 	while (i < n && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = 0;
 	return (dest);
 }
