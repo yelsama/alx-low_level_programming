b62fa160c7300bb9bdc0ff070a1b626b72dd0fe7
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-02 19:10:07 +0400

    up

---
+++ 0x06-pointers_arrays_strings/2-stncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - check the code
+ * @dest: a placr to copy
+ * @src: place to take string from
+ * @n: number of characters to copy
+ * Return: the destenation
+ */
+char	*_strncpy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
