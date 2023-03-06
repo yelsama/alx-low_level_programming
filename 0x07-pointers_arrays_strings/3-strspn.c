759490ee2356466f0d8b966fbe98b1a8678d6814
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r3s2.42abudhabi.ae>
2023-03-06 16:17:35 +0400

    up

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - check the code
+ * @s: string to to span
+ * @accept: set of strings to look at
+ * Return: 
+ */
+unsigned int	_strspn(char *s, char *accept)
+{
+	unsigned int	i;
+	char			*tmp;
+
+	i = 0;
+	while (s[i])
+	{
+		tmp = accept;
+		while (*tmp)
+		{
+			if (*tmp != s[i])
+				return (i);
+			tmp++;
+		}
+		i++;
+	}
+	return (i);
+}
