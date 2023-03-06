f6400f1d1a8cde50517b7a740b600d717b2592a8
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r5s10.42abudhabi.ae>
2023-03-06 18:17:00 +0400

    up

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - check the code
  * @s: string to to span
  * @accept: set of strings to look at
- * Return: 
+ * Return: index or similar ones
  */
 unsigned int	_strspn(char *s, char *accept)
 {
@@ -28,16 +28,4 @@ unsigned int	_strspn(char *s, char *accept)
 		i++;
 	}
 	return (i);
-}
-
-#include <stdio.h>
-#include <string.h>
-int main() {
-    char s[] = "hello world";
-    char accept[] = "helo w";
-    unsigned int count = _strspn(s, accept);
-    printf("The initial segment of '%s' that consists entirely of characters from '%s' has length %u.\n", s, accept, count);
-	count = strspn(s, accept);
-    printf("The initial segment of '%s' that consists entirely of characters from '%s' has length %u.\n", s, accept, count);
-    return 0;
 }
