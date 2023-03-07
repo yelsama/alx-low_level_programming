0df65809d01c67e31307c4855ba5823c27cb8327
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r5s14.42abudhabi.ae>
2023-03-07 18:42:44 +0400

    up

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - check the code
+ * @s: address of string to be modified
+ * @to: string reference
+ */
+void	set_string(char **s, char *to)
+{
+	*s = to;
+}
