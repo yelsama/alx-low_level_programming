999826d8c4fa1e22d7bf9ed485efd513659845a7
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-08 11:02:49 +0400

    up

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: string to print
+ */
+void	_puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s)
+		_puts_recursion(s);
+}
