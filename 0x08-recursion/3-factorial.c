fc64c6cbe643f42ab7e8c59647e8bd40e42821ae
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-08 11:26:15 +0400

    up

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial- check the code
+ * @n: given number to get factorial
+ * Return: factorial of n
+ */
+int	factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1 || n == 0)
+		return (1);
+	return (n * ft_recursive_factorial(n - 1));
+}
