f9dd4e15d0f4a23fe4958ac59401f1eaac0e3a6b
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-08 11:32:58 +0400

    up

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * factorial- check the code
+ * _pow_recursion - check the code
  * @x: given base number
  * @y: the power number
  * Return: result of x^y
@@ -9,7 +9,7 @@
 int	_pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (0);
+		return (-1);
 	if (y == 0)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
