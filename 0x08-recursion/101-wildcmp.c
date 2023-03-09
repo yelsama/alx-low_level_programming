59562f931424afdf1758fd8bfa54e472c2db0868
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-09 20:06:09 +0400

    up

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -44,13 +44,13 @@ int	test_strings(char *s1, char *s2, int *i, int *j)
 		*j += 1;
 		return (test_strings(s1, s2, i, j));
 	}
+	else if (s1[*i] != s2[*j] && s2[*j] != '*')
+		return (0);
 	else if (s2[*i] == '*')
 	{
 		set_balance(s1, s2, i, j);
 		return (test_strings(s1, s2, i, j));
 	}
-	else if (s1[*i] != s2[*j] && s2[*j] != '*')
-		return (0);
 	return (1);
 }
 
