e17b673127143cc1221e2091175c71da506b8eec
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-09 13:26:47 +0400

    up

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * ft_is_prime - check the code
+ * @nb: given number to get square root
+ * Return: 1 if prime, otherwise 0
+ */
+int	ft_is_prime(int nb)
+{
+	int	n;
+
+	n = nb - 1;
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	while (n > 0)
+	{
+		if (nb % n == 0)
+			break ;
+		n--;
+	}
+	if (n == 1)
+		return (1);
+	return (0);
+}
+
+/**
  * is_prime_number - check the code
  * @n: given number to get square root
  * Return: 1 if prime, otherwise 0
@@ -8,9 +33,10 @@
 int	is_prime_number(int n)
 {
 	int	m;
+
 	if (n == 2)
 		return (1);
 	if (n % 2 == 0 || n <= 0)
 		return (0);
-	return (n / (is_prime_number()))
+	return (ft_is_prime(n));
 }
