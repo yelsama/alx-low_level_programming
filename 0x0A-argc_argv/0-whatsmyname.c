46f6066a3145ad6cce05a47789438d88b9c9c59a
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-10 23:47:34 +0400

    up

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * main - to print its nmae
+ * main - to print its name
+ * @argc: number of input args
+ * @argv: tow d string
  * Return: zero when finished
  */
 int	main(int argc, char **argv)
