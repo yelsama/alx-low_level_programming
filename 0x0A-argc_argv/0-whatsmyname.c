6deb5c270df5643761cbcf95ef01f6251c8b516a
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-10 23:35:29 +0400

    up

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - to print its nmae
+ * Return: zero when finished
+ */
+int	main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	printf("\n");
+	return (0);
+}
