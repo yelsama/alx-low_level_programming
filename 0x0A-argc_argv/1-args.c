f12f9aba8be870b860fe0b6d7d31faaac7553a3e
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-10 23:49:24 +0400

    up

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - to print num of args
+ * @argc: number of input args
+ * @argv: tow d string
+ * Return: zero when finished
+ */
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
