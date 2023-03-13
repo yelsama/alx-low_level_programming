8da8ad3272fe8a0f230bc0ad914f815557130373
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-14 01:49:38 +0400

    up

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - to print num of args
+ * @size: size of string
+ * @c: the char to fill string with
+ * Return: a pointer to the created string
+ */
+char	*create_array(unsigned int size, char c)
+{
+	char	*str;
+	int		i;
+
+	if (!size)
+		return (NULL);
+	str = malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (++i < (int)size)
+		str[i] = c;
+	str[i] = 0;
+	return (str);
+}
