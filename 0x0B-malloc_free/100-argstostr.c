96797c520d7887aa0985938e83da54e35ad650ff
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-14 16:27:47 +0400

    up

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * argstostr - put args into one string
+ * @ac: number of argument
+ * @av: args as two dim array
+ * Return: a pointer to the new string
+ */
+char	*argstostr(int ac, char **av)
+{
+	char	*new;
+	int		i;
+	int		n;
+	int		t_n;
+
+	if (!av || ac < 1)
+		return (NULL);
+	i = -1;
+	t_n = 0;
+	while (++i < ac)
+	{
+		n = -1;
+		while (av[i][++n])
+			t_n++;
+	}
+	new = malloc(sizeof(char) * (t_n + ac));
+	if (!new)
+		return (NULL);
+	i = -1;
+	t_n = -1;
+	while (++i < ac)
+	{
+		n = -1;
+		while (av[i][++n])
+			new[++t_n] = av[i][n];
+		new[++t_n] = ' ';
+	}
+	new[t_n] = 0;
+	return (new);
+}
