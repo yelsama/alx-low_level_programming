a3d66da147137c56c8b0a8967609e17190ede381
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-16 20:09:45 +0400

    up

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
 void	*_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned char	*new;
-	unsigned char	tmp;
+	unsigned char	*tmp;
 	unsigned int	i;
 
 	tmp = (unsigned char *)ptr;
