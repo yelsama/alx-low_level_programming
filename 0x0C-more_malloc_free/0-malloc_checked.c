f2d4766370675cb20d104b90c2ecd2f65569c4a1
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-16 17:02:04 +0400

    up

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - insure memory allocation
+ * @b: number of bytes to reserve
+ * Return: address fo createc memory
+ */
+void	*malloc_checked(unsigned int b)
+{
+	void	*mem;
+
+	mem = malloc(b);
+	if (!mem)
+		exit(89);
+	return (mem);
+}
