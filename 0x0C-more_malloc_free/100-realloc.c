5f19e6da314642f1800d8614ffd6acc16db75359
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-16 20:06:50 +0400

    up

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _realloc - expand a a memory;
+ * @ptr: pointer to original memory
+ * @old_size: name
+ * @new_size: name
+ * Return: a pointer to the new memory allocated
+ */
+void	*_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned char	*new;
+	unsigned char	tmp;
+	unsigned int	i;
+
+	tmp = (unsigned char *)ptr;
+	if (new_size == 0)
+	{
+		if (ptr)
+			free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	new = malloc(new_size);
+	if (!new)
+		return (NULL);
+	i = -1;
+	if (ptr)
+		while (++i < new_size && i < old_size)
+			new[i] = tmp[i];
+	if (ptr)
+		free(ptr);
+	return (new);
+}
