403ee192dee87d03ed495035291a4c15f3a30773
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-20 20:39:37 +0400

    up

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,31 @@
 #include "dog.h"
 
 /**
+ * _strdup - to print num of args
+ * @str: string to duplicate
+ * Return: a pointer to the new string
+ */
+char	*_strdup(char *str)
+{
+	char	*new;
+	int		i;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (str[i])
+		i++;
+	new = malloc(sizeof(char) * (i + 1));
+	if (!new)
+		return (NULL);
+	i = -1;
+	while (str[++i])
+		new[i] = str[i];
+	new[i] = str[i];
+	return (new);
+}
+
+/**
  * new_dog - see code
  * @name: an argue
  * @age: an argue
@@ -14,8 +39,8 @@ dog_t	*new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t) * 1);
 	if (!d)
 		return (NULL);
-	d->name = strdup(name);
+	d->name = _strdup(name);
 	d->age = age;
-	d->owner = strdup(owner);
+	d->owner = _strdup(owner);
 	return (d);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,6 @@
 # define DOG_H
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 
 /**
  * struct dog - Represents a dog
