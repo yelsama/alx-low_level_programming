8beafa54822bf4fb939ba1899e7c7e86f30d75cc
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-20 19:57:31 +0400

    up

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void	print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	printf("Age: %f\n", d->age);
 	if (d->owner)
-		printf("Owner: %s", d->owner);
+		printf("Owner: %s\n", d->owner);
 	else
 		printf("Owner: (nil)\n");
 }
