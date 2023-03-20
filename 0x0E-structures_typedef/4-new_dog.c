2d6c3182a9d4035ce45762fb95e535ed9e7b66c9
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-20 20:18:14 +0400

    up

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,17 +5,17 @@
  * @name: an argue
  * @age: an argue
  * @owner: an argue
- * Return: new struct dog
+ * Return: new struct
  */
 dog_t	*new_dog(char *name, float age, char *owner)
 {
-	dog_t	d;
+	dog_t	*d;
 
-	// d = malloc(sizeof(dog_t));
-	// if (!d)
-	// 	return (NULL);
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
-	return (&d);
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
 }
