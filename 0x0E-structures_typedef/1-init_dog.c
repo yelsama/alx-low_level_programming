d70b79090e808aec6447a9a44073e01f0401da52
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-20 19:25:24 +0400

    up

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - see code
+ * @d: struct to fill
+ * @name: an argue
+ * @age: an argue
+ * @owner: an argue
+ */
+void	init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
