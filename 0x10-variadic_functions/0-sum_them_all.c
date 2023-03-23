1a8748a200377624492910c696e627009dfe04e3
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-23 21:22:53 +0400

    up

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,15 +7,15 @@
  */
 int	sum_them_all(const unsigned int n, ...)
 {
-	va_list 	ap;
+	va_list	ap;
 	unsigned int i, sum;
 
 	if (n == 0)
 		return (0);
-	va_start (ap, n);
+	va_start(ap, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
-	va_end (ap);
+		sum += va_arg(ap, int);
+	va_end(ap);
 	return (sum);
 }
