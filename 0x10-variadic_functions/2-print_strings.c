74a05f0f57773140b68fd7170e2825a30ea742bf
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-23 23:55:04 +0400

    up

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print n of strings
+ * @separator: string to put bettween numbers
+ * @n: number of given string
+ */
+void	print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list			ap;
+	char			*tmp;
+	unsigned int	i;
+
+	va_start(ap, n);
+	i = 0;
+	while (i < n)
+	{
+		tmp = va_arg(ap, char *);
+		if (i == 0 || !separator)
+		{
+			if (!tmp)
+				printf("(nil)");
+			else
+				printf("%s", tmp);
+		}
+		else
+		{
+			if (!tmp)
+				printf("%s(nil)", separator);
+			else
+				printf("%s%d", separator, tmp);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
