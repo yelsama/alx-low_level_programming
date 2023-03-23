a149762b11f9ce5c5ce16d8ecd9892b34a921c9f
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-23 19:40:25 +0400

    up

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+"variadic_functions.h"
+
+/**
+ * sum_them_all - submint all given intgers
+ * @n: number of given intgers
+ * Return: sumution of all
+ */
+int	sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	if (n == 0)
+		return (0);
+	va_start (ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg (ap, int);
+	va_end (ap);
+	return (sum);
+}
+
