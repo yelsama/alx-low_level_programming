6409b5f20b4364929dd78778fe2c2c8c560f9601
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-24 02:04:23 +0400

    up

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - check the code
+ * @name: a given name to print
+ * @f: pointer to printing function 
+ */
+void	print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
