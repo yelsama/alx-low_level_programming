0b9d7dbf47bd75b04dfb4d5e60c3632dc96b6cc1
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-24 18:25:06 +0400

    up

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,24 +6,6 @@
  * @argv: arguments strings
  * Return: 0 when success
  */
-// int	main(int argc, char **argv)
-// {
-// 	int	n1, n2;
-// 	int	(*res)(int, int);
-
-// 	if (argc != 4)
-// 		return (printf("Error\n"), 98);
-// 	res = NULL;
-// 	n1 = atoi(argv[1]);
-// 	n2 = atoi(argv[3]);
-// 	res = get_op_func(argv[2]);
-// 	if (!res)
-// 		return (printf("Error\n"), 99);
-// 	if ((res == op_div || res == op_mod) && n2 == 0)
-// 		return (printf("Error\n"), 100);
-// 	printf("%d\n", res(n1, n2));
-// 	return (0);
-// }
 int main(int argc, char *argv[])
 {
 	int one, two, ans;
