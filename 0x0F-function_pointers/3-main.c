0f2cb92d655bbf06a717cb33a7d419f97be7ac45
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-24 18:04:39 +0400

    up

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+
+/**
+ * main - gets numbers from args and manipulate them
+ * @argc: number of arguments
+ * @argv: arguments strings
+ * Return: 0 when success
+ */
+int	main(int argc, char **argv)
+{
+	int	n1,n2;
+	int	(*res)(int, int);
+
+	if (argc != 4)
+		return (printf("Error\n"), 98);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	res = get_op_func(argv[2]);
+	if (!res)
+		return (printf("Error\n"), 99);
+	if ((res == op_div || res == op_mod) && n2 == 0)
+		return (printf("Error\n"), 100);
+	printf ("%d\n", res(n1, n2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds tow intgers
+ * @a: first int
+ * @b: secont int
+ * Return: result of adding
+ */
+int	op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub tow intgers
+ * @a: first int
+ * @b: secont int
+ * Return: result of sub
+ */
+int	op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mulitpilcate tow intgers
+ * @a: first int
+ * @b: secont int
+ * Return: result of multiplication
+ */
+int	op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide first by second
+ * @a: first int
+ * @b: secont int
+ * Return: result of division
+ */
+int	op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulate first by second
+ * @a: first int
+ * @b: secont int
+ * Return: result of modulation
+ */
+int	op_mod(int a, int b)
+{
+	return (a % b);
+}
