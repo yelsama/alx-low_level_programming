56a9e11040dd3d46dcb5fe72e9a84ef1eae897bb
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-24 18:28:45 +0400

    up

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,37 +6,20 @@
  * @argv: arguments strings
  * Return: 0 when success
  */
-int main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int	n1, n2;
+	int	(*res)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	res = get_op_func(get_op);
-	ans = res(one, two);
-
-	printf("%d\n", ans);
+		return (printf("Error\n"), 98);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	res = get_op_func(argv[2]);
+	if (!res)
+		return (printf("Error\n"), 99);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && n2 == 0)
+		return (printf("Error\n"), 100);
+	printf("%d\n", res(n1, n2));
 	return (0);
 }
