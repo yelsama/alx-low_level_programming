afb20e6c917ab9ebdc334aeaf55b88abcdd7509b
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-24 18:31:14 +0400

    up

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,14 +12,23 @@ int	main(int argc, char **argv)
 	int	(*res)(int, int);
 
 	if (argc != 4)
-		return (printf("Error\n"), 98);
+	{
+		printf("Error\n");
+		return (98);
+	}
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 	res = get_op_func(argv[2]);
 	if (!res)
-		return (printf("Error\n"), 99);
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && n2 == 0)
-		return (printf("Error\n"), 100);
+	{
+		printf("Error\n");
+		return (99);
+	}
 	printf("%d\n", res(n1, n2));
 	return (0);
 }
