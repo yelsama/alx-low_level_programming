6670fd48e46d5346c05848962c20695b523f5ed7
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-30 17:17:32 +0400

    up

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,8 +7,9 @@
  */
 void	free_list(list_t *head)
 {
-	if (head->next)
-		free_list(head->next);
+	if (!head)
+		return;
+	free_list(head->next);
 	free(head->str);
 	head->str = NULL;
 	free(head);
