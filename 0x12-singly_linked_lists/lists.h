4961dfb96c291cb07ef17fa9168450d3cf497458
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-30 16:04:13 +0400

    up

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,14 @@
 
 # include <stdio.h>
 
+/**
+ * struct list_t - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_t
 {
 	char	*str;
