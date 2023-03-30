fd8e06b20a9ac0f72fc3eabdfa9d35f24c1193c0
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-03-30 16:51:46 +0400

    up

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node_end - prints list elements
+ * @head: holds the pointer to the first element
+ * @str: string to duplicated in the node
+ * Return: the head for the node
+ */
+list_t	*add_node_end(list_t **head, const char *str)
+{
+	list_t	*new_tail;
+	list_t	*trace;
+	int		i;
+
+	trace = *head;
+	new_tail = malloc(sizeof(list_t));
+	if (!new_tail)
+		return (NULL);
+	new_tail->str = strdup(str);
+	i = 0;
+	while (str && str[i])
+		i++;
+	new_tail->len = i;
+	while (trace->next)
+		trace = trace->next;
+	trace->next = new_tail;
+	return (*head);
+}
