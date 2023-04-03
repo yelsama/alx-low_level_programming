9bd609d801504c1749813709933a3943bfedbd35
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-03 23:44:28 +0400

    up

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - prints list elements
+ * @head: holds the pointer to the first element
+ * @n: intger to hold
+ * Return: the head for the node
+ */
+listint_t	*add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t	*new_tail;
+	listint_t	*trace;
+
+	new_tail = malloc(sizeof(listint_t));
+	if (!new_tail)
+		return (NULL);
+	new_tail->n = n;
+	trace = *head;
+	if (!trace)
+	{
+		*head = new_tail;
+		return (new_tail);
+	}
+	while (trace->next)
+		trace = trace->next;
+	trace->next = new_tail;
+	return (new_tail);
+}
