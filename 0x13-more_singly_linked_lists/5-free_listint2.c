741361e5e8989c6303029af78df2f77ea662431a
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 00:08:03 +0400

    up

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
 /**
- * free_listint - free a list
+ * free_listint2 - free a list
  * @head: holds the pointer to the first element
  * Return: no thing
  */
-void	free_listint2(listint_t **head);
+void	free_listint2(listint_t **head)
 {
 	if (!head)
 		return;
-	free_list(head->next);
-	free(head);
+	free_listint(*head);
 	head = NULL;
 }
