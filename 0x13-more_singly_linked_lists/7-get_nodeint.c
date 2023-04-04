b4d72ef398bbcaaacdcdaa82a53c6e46d7e79bde
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 04:36:03 +0400

    up

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t	*get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int	i;
 
 	i = 0;
-	while (head && i++ <= index)
+	while (head && ++i <= index)
 		head = head->next;
 	return (head);
 }
