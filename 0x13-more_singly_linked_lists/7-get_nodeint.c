89b9e6ea55e334cd25fc4cdaa504f45b0b9b7820
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 04:34:14 +0400

    up

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - find and return
+ * @head: holds the pointer to the first element
+ * @index: inteded index to return
+ * Return: link to the node if exits
+ */
+listint_t	*get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (head && i++ <= index)
+		head = head->next;
+	return (head);
+}
