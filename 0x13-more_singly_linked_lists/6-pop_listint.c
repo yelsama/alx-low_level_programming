be1481319a0cb821ba74e6a3e233c0cd33bc48c1
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 04:24:00 +0400

    up

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,7 @@ int	pop_listint(listint_t **head)
 		return (0);
 	new_head = (*head)->next;
 	free(*head);
-	*head = new_head;
+	head = &new_head;
 	if (!new_head)
 		return (0);
 	return (new_head->n);
