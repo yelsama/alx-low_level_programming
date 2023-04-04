b82dcba1f06e96671aac692007a490216ec0a1bf
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 05:43:23 +0400

    up

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head)
 		return (NULL);
-	if (!(*head) && n > 0)
+	if (!(*head) && idx > 0)
 		return (NULL);
 	tmp = *head;
 	i = 0;
