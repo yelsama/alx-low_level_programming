f7c09cb83ec469284d2f05c73ed3764e0636af74
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 07:03:33 +0400

    up

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,7 @@ listint_t	*add_nodeint_end(listint_t **head, const int n)
 	listint_t	*new_tail;
 	listint_t	*trace;
 
+	trace = NULL;
 	new_tail = malloc(sizeof(listint_t));
 	if (!new_tail)
 		return (NULL);
