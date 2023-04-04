2c6247173b6c666ac9d454a1057e92a8fbb1cced
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 05:39:33 +0400

    up

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,13 +19,14 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	tmp = *head;
 	i = 0;
-	while (tmp && ++i <= idx)
+	while (tmp && ++i < idx)
 		tmp = tmp->next;
-	if (i <= idx)
+	if (i < idx)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
+	new_node->n = n;
 	new_node->next = tmp->next;
 	tmp->next = new_node;
 	return (new_node);
