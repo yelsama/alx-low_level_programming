30a346e1c86ede6adbbdaaad48f38e0b259beab3
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 08:35:25 +0400

    up

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int	delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	tmp = *head;
 	i = 0;
-	while (tmp && ++i < index)
+	while (tmp && i++ < index)
 		tmp = tmp->next;
 	to_del = tmp->next;
 	if (!index)
