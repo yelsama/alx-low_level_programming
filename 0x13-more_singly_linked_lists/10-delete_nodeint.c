1375ace8cafd03ee1def3fa65c1ece090f31827d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-04 06:54:19 +0400

    up

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,25 +8,26 @@
  */
 int	delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t	*new_node;
 	listint_t	*tmp;
+	listint_t	*to_del;
 	unsigned int	i;
 
-	if (!head)
-		return (NULL);
-	if (!(*head) && idx > 0)
-		return (NULL);
+	if (!head || !(*head))
+		return (-1);
 	tmp = *head;
 	i = 0;
-	while (tmp && ++i < idx)
+	while (tmp && ++i < index)
 		tmp = tmp->next;
-	if (i < idx)
-		return (NULL);
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = tmp->next;
-	tmp->next = new_node;
-	return (new_node);
+	to_del = tmp->next;
+	if (!index)
+	{
+		to_del = *head;
+		*head = to_del->next;
+	}
+	if (!to_del)
+		return (-1);
+	tmp->next = to_del->next;
+	free(to_del);
+	to_del = NULL;
+	return (1);
 }
