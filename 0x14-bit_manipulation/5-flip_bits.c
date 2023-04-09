849d7191cc68cba9f31a0736f8fad118787eef19
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-10 03:31:41 +0400

    up

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - prints binary
+ * @n: number to be checked
+ * @m: bit index to convert to one
+ * Return: count of flip
+ */
+unsigned int	flip_bits(unsigned long int n, unsigned long int m)
+{
+    unsigned int count = 0;
+    unsigned long int diff = n ^ m;
+
+    while (diff) {
+        count += diff & 1;
+        diff >>= 1;
+    }
+    
+    return count;
+}
