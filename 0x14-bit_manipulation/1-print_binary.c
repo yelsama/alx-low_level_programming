768d777227275a837b6887ca5eec0b537724aea0
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-10 01:00:58 +0400

    up

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,8 @@
 void	print_binary(unsigned long int n)
 {
 	if (!n)
-	{
-		_putchar('0');
 		return;
-	}
 	if (n)
 		print_binary(n >> 1);
-	if (n)
-		_putchar((n & 1) + '0');
+	_putchar((n & 1) + '0');
 }
