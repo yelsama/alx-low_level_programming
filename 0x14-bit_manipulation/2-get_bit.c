1c3337b88ae994ab20c66bddc620cac19ffd3b86
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-10 01:28:50 +0400

    up

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - prints binary
+ * @n: number to be checked
+ * @index: bit index to investigate
+ * Return: binary value of the bit
+ */
+int	get_bit(unsigned long int n, unsigned int index)
+{
+	if (n == 0 && index > 0)
+		return (-1);
+	if (n == 0 && index == 0)
+		return (0);
+	while (index > 0 && n)
+	{
+		n >> 1;
+		index--;
+	}
+	if (!n)
+		return (-1);
+	return (n & 1);
+}
