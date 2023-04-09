13272969d2a8c24c4b333b901a1c2038b45cda21
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-09 06:25:08 +0400

    up

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+
+/**
+ * binary_to_uint - prints list elements
+ * @b: string to be converted
+ * Return: dicimal number
+ */
+unsigned int	binary_to_uint(const char *b)
+{
+	int				n;
+	unsigned int	ret;
+	unsigned int	base;
+
+	n = 0;
+	ret = 0;
+	base = 1;
+	if (!b || !b[0])
+		return (0);
+	while (b[n])
+		n++;
+	while (--n >= 0)
+	{
+		if (b[n] < '0' || b[n] > '1')
+			return (0);
+		ret += base * (b[n] - '0');
+		base *= 2;
+	}
+	return (ret);
+}
