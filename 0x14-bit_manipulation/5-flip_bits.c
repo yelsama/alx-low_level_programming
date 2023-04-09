e19aa34f9caada3a65c7bb784d73feb4f309d9dd
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-10 03:35:02 +0400

    up

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,13 +8,13 @@
  */
 unsigned int	flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned int count = 0;
-    unsigned long int diff = n ^ m;
+	unsigned int		count = 0;
+	unsigned long int	diff = n ^ m;
 
-    while (diff) {
-        count += diff & 1;
-        diff >>= 1;
-    }
-    
-    return count;
+	while (diff)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+	return (count);
 }
