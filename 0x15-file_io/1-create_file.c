7db53c75c35bcc6dcf2298ac47b67687919eee8c
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-11 20:00:26 +0400

    up

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,15 +29,15 @@ int	create_file(const char *filename, char *text_content)
 	int		fd;
 	ssize_t	bytes_written;
 
-	if (filename == NULL || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd == -1)
+	if (fd < 1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 	{
 		bytes_written = write(fd, text_content, _strlen(text_content));
-		if (bytes_written == -1)
+		if (bytes_written < 1)
 		{
 			close(fd);
 			return (-1);
