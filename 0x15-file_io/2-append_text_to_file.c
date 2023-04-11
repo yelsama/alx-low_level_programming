8ad1f34e287590d849f73ceb3d11dfdca0971439
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-04-11 21:59:41 +0400

    up

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,8 +11,10 @@ int	append_text_to_file(const char *filename, char *text_content)
 	int	fd;
 	int	i, n;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
+	if (!text_content)
+		return (1);
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd < 1)
 		return (-1);
