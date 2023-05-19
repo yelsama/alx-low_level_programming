322a6e1da9230f5a6454abd35a520b46d827429e
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-19 19:50:31 +0400

    up

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - prints list elements
+ * @head: holds the pointer to the first element
+ * @n: number to be stored at the node
+ * Return: the new head for the node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t	*new_head;
+	int			i;
+
+	new_head = malloc(sizeof(dlistint_t));
+	if (!new_head)
+		return (NULL);
+	new_head->n = n;
+	new_head->next = *head;
+	(*head)->prev = new_head;
+	new_head->prev = NULL;
+	*head = new_head;
+	return (new_head);
+}
