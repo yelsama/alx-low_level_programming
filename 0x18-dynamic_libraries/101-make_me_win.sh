47b510439c93842c8a4c0e1e63d4b901767a7761
Yassein Eltayeb Mohamed Idris <ymohamed@lab2r2s10.42abudhabi.ae>
2023-05-22 21:53:18 +0400

    up

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp https://github.com/yelsama/alx-low_level_programming/blob/master/0x18-dynamic_libraries/yasoWins.so
-export LD_PRELOAD=/tmp/yasoWins.so
+LD_PRELOAD=https://github.com/yelsama/alx-low_level_programming/blob/master/0x18-dynamic_libraries/yasoWins.so ./gm
