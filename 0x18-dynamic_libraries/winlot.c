14896d46d91fa5a91382e4d4ab7ce8ffd42d96c5
Yassein Eltayeb Mohamed Idris <ymohamed@lab2r2s10.42abudhabi.ae>
2023-05-22 22:02:56 +0400

    up

--- 0x18-dynamic_libraries/winlot.c
+++ 0x18-dynamic_libraries/winlot.c
@@ -4,20 +4,21 @@
 //9 8 10 24 75 9
 int rand()
 {
-	static int n = -1;
+	static int n;
 
 	n++;
-	if (n == 0)
-		return 9;
 	if (n == 1)
-		return 8;
-	if (n == 2)
-		return 10;
-	if (n == 3)
-		return 24;
-	if (n == 4)
-		return 75;
-	if (n == 5)
 		return 9;
-	return n * n % 30000;
+	if (n == 2)
+		return 8;
+	if (n == 3)
+		return 10;
+	if (n == 4)
+		return 24;
+	if (n == 5)
+		return 75;
+	if (n == 6)
+		return 9;
+	n = 1;
+	return 9;
 }
