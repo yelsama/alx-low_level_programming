147c54b29ba03a8d0e111e324ec1a42c85bc5b5d
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-09 23:03:40 +0400

    up

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - check the code
+ * @ht: hash table pointer
+ * @key: a key to find the element 
+ * Return: a vlue for a key or none
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+
+	if (!ht || !key || !key[0])
+		return (NULL);
+	i = key_index((const unsigned char *)(key), (*ht).size);
+	tmp = (*ht).array[i];
+	while (tmp)
+	{
+		if (!strcmp((*tmp).key, key))
+			return ((*tmp).value);
+		tmp = (*tmp).next;
+	}
+	return (NULL);
+}
