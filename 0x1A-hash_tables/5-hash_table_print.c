b5222d9d1d9635c37c88e67d5662ff5f5e919188
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-09 23:34:14 +0400

    up

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,8 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			printf("'%s': '%s'", (*tmp).key, (*tmp).value);
 			tmp = (*tmp).next;
-			printf(", ");
+			if (tmp || (*ht).array[i + 1])
+				printf(", ");
 		}
 		i++;
 	}
