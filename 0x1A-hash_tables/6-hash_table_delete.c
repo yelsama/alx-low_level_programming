8a08d7b2cff234e555b5e3f4880d2fbc2b451017
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-10 00:04:42 +0400

    up

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+
+
+/**
+ * free_list - free a list
+ * @head: holds the pointer to the first element
+ * Return: no thing
+ */
+void	free_list(hash_node_t *head)
+{
+	if (!head)
+		return;
+	free_list(head->next);
+	free(head->value);
+	free(head->key);
+	free(head);
+	head = NULL;
+}
+
+/**
+ * hash_table_delete - check the code
+ * @ht: pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i = 0;
+
+	if (!ht)
+		return;
+	while (i < (*ht).size)
+	{
+		tmp = (*ht).array[i];
+		free_list(tmp);
+		i++;
+	}
+	free(ht);
+	ht = NULL;
+}
