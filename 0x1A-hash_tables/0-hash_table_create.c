3a23c3a95b6ef9687ed4efcace8a8188b9ae8bdb
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-09 11:36:54 +0400

    up

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+
+
+/**
+ * hash_table_create - check the code 
+ * 
+ * @size: inted size for hash table 
+ * return: hash_table_t* 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	new_table = NULL;
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	return (new_table);
+}
+q
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -30,5 +32,5 @@ typedef struct hash_table_s
      hash_node_t **array;
 } hash_table_t;
 
-
+hash_table_t *hash_table_create(unsigned long int size);
 #endif
