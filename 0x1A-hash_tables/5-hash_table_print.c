e1f2fff93b92f7524d4f3f582facaf63d8b90703
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s11.42abudhabi.ae>
2023-06-09 23:18:17 +0400

    up

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_print
+ *
+ * @ht: pointer to the hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	int i = -1;
+
+	if (!ht)
+		return;
+	tmp = (*ht).array[++i];
+	printf("{");
+	while (tmp)
+	{
+		printf("'%s'", (*tmp).value);
+		tmp = (*tmp).next;
+		if (tmp)
+			printf(": ");
+	}
+	printf("}\n");
+}
